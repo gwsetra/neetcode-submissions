84a6c799b15593115c1494edbb5888cc91937048
Setra Wicana <gw.setra@gmail.com>
2026-06-21 20:42:45 +0100

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # print(image)
+        # print(sr, sc)
+        ori_value = image[sr][sc]
+        def dfs(image, curr_row, curr_col, color):
+            print('current position', curr_row, curr_col)
+            if (min(curr_row, curr_col) < 0 
+                or (curr_row == len(image[0])) or curr_col == len(image)
+                or image[curr_row][curr_col] != ori_value
+                or image[curr_row][curr_col] == color):
+                print('return')
+                return
+            elif image[curr_row][curr_col] == ori_value:
+                print('continue process, update value')
+                print('current image', image)
+                image[curr_row][curr_col] = color
+                print('after updated image', image)
+
+                dfs(image, curr_row-1, curr_col, color) # atas
+                dfs(image, curr_row, curr_col+1, color) # kanan
+                dfs(image, curr_row+1, curr_col, color) # bawah
+                dfs(image, curr_row, curr_col-1, color) # bawah
+
+
+        dfs(image, sr, sc, color)
+
+        return image    
+        
