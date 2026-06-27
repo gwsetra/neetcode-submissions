f12f272ad7751d117b9c3fbd6bcaa6f56254d37b
Setra Wicana <gw.setra@gmail.com>
2026-06-27 17:31:31 +0100

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        max_row = len(grid)
+        max_col = len(grid[0])
+        fresh_checker = set()
+        visited = set()
+        queue = deque()
+        num_of_minutes = -1 
+        directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+        def bfs(row, count):
+            nonlocal num_of_minutes
+
+            print('start bfs', row, count)
+            queue.append((row, count))
+            visited.add((row, count))
+
+            while len(queue):
+                print('content of queue', queue)
+                for loop in range(len(queue)):
+                    r, c = queue.popleft()
+
+                    for dr, dc in directions:
+
+                        if (min(r+dr, c+dc) < 0 or
+                        r+dr == max_row or c+dc == max_col 
+                        or grid[r+dr][c+dc] == 0 or
+                        (r+dr, c+dc) in visited ):
+                            continue
+
+                        if grid[r+dr][c+dc] == 1:
+                            queue.append((r+dr, c+dc))
+                            visited.add((r+dr, c+dc))
+
+                num_of_minutes += 1
+                    
+        
+        for loopr in range(max_row):
+            for loopc in range(max_col):
+                if grid[loopr][loopc] == 1:
+                    fresh_checker.add((loopr, loopc))
+                if grid[loopr][loopc] == 2:
+                    print('rotten found', loopr, loopc)
+                    bfs(loopr, loopc)
+        
+        print(fresh_checker)
+        print(visited)
+        return num_of_minutes if fresh_checker.issubset(visited) else -1
+
+                    
