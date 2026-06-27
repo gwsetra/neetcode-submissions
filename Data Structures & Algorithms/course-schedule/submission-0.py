616fe88667e5984a744a67d94f740688fa335c5a
Setra Wicana <gw.setra@gmail.com>
2026-06-27 23:23:44 +0100

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        ## build graph
+        graph = {}
+        prereqlen = len(prerequisites)
+
+        print(prerequisites)
+
+        for item in prerequisites:
+            if item[0] not in graph:
+                graph[item[1]] = item[0]
+        
+        print(len(graph))
+        print(graph)
+
+        if prereqlen != len(graph):
+            return False
+        elif numCourses == len(graph)*2:
+            return True
+        else:
+            return False
