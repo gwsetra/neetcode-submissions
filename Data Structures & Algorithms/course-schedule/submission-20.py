d8997f3da703e42a9ddb9fe40492bbbe3bb2e034
Setra Wicana <gw.setra@gmail.com>
2026-06-28 14:22:44 +0100

    Add: course-schedule - submission-20

---
+++ Data Structures & Algorithms/course-schedule/submission-20.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjlist = {}
+        visited = set()
+
+        def find_cycle(v, edges):
+            if not edges:
+                return True
+
+            for e in edges:
+                if v in visited:
+
+                    return False
+                visited.add(v)
+                if not find_cycle(e, adjlist[e]):
+                    return False
+                visited.remove(v)
+            
+            return True
+
+        def check_all_element():
+            for key, values in adjlist.items():
+                if find_cycle(key, values) == False:
+                    return False
+            return True
+
+        for item in range(numCourses):
+            if item not in adjlist:
+                adjlist[item] = []
+        
+        for item in prerequisites:
+            if item[1] == item[0]:
+                return False
+            adjlist[item[1]].append(item[0])
+
+        return check_all_element()
