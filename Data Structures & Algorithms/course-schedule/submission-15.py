c55dcba3a39cb7bae7de186804f6973a91b7c9ed
Setra Wicana <gw.setra@gmail.com>
2026-06-28 14:00:09 +0100

    Add: course-schedule - submission-15

---
+++ Data Structures & Algorithms/course-schedule/submission-15.py
@@ -0,0 +1,43 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjlist = {}
+        visited = set()
+        loop_not_found = True
+
+        def find_cycle(v, edges):
+            nonlocal loop_not_found
+            print('inside find_cycle', v, edges)
+            for e in edges:
+                print(v, e)
+                if (v, e) in visited:
+                    print('inside loop not')
+                    loop_not_found = False
+
+                    return False
+                visited.add((v, e))
+                return find_cycle(e, adjlist[e])
+                visited.remove((v, e))
+            
+            # return True
+
+        def check_all_element():
+            for key, values in adjlist.items():
+                if loop_not_found:
+                    return find_cycle(key, values)
+            return False
+
+        
+
+        for item in range(numCourses):
+            if item not in adjlist:
+                adjlist[item] = []
+        
+
+        for item in prerequisites:
+            if item[1] == item[0]:
+                return False
+            adjlist[item[1]].append(item[0])
+        print(adjlist)
+
+
+        return check_all_element()
