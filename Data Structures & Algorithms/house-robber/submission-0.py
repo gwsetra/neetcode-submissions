e6ebda1d2279a381b63907cb04ef3559a8ba3050
Setra Wicana <gw.setra@gmail.com>
2026-07-03 22:38:24 +0100

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        is_even = (len(nums) % 2) == 0
+        max_odd = 0
+        max_even = 0
+
+        for i in range(len(nums)):
+            # print(i)
+            if i % 2 == 0:
+                max_even += nums[i]
+            else:
+                max_odd += nums[i]
+        
+        return max_even if max_even >= max_odd else max_odd
