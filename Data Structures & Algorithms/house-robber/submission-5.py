caebe79a80f0f125a9e4f78c705d5d89eb6df6b9
Setra Wicana <gw.setra@gmail.com>
2026-07-04 00:08:24 +0100

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        # print(nums)
+        def dfs(is_skip, curidx):
+            # print('------**----')
+            # print(is_skip, curidx)
+            if curidx in cache:
+                return cache[curidx]
+            if curidx+1 > len(nums)-1 or curidx+2 > len(nums)-1:
+                print('im returning', nums[curidx])
+                if curidx not in cache:
+                    cache[curidx] = nums[curidx]
+                return nums[curidx]
+
+            # print('going into another dfs')
+
+            current_max = max(0 + dfs(True, curidx+1), nums[curidx]+dfs(False, curidx+2))
+            # print('after dfs current_max', current_max)
+            # print(is_skip, curidx)
+            cache[curidx] = current_max
+            return  current_max
+
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        return dfs(False, 0)
