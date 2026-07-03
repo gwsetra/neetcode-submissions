9aafec6b62009d2a7ec4ad5058f0f1432b974996
Setra Wicana <gw.setra@gmail.com>
2026-07-03 22:27:52 +0100

    Add: climbing-stairs - submission-19

---
+++ Data Structures & Algorithms/climbing-stairs/submission-19.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+    # space optimised
+        cache = {}
+        cache[0] = 1
+        cache[1] = 1
+        tmp = 0
+
+        print(n, cache)
+
+        for loop in range(2, n+1):
+            tmp = cache[0] + cache[1]
+            cache[0] = cache[1]
+            cache[1] = tmp
+        return tmp
