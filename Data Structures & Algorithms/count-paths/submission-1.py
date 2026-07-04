719a6882980f52501cb72becbe5c587b45dc3f83
Setra Wicana <gw.setra@gmail.com>
2026-07-04 17:21:39 +0100

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # print(m, n)
+        prev = [0]*n
+        curr = [0]*n
+        # print(prev, curr)
+
+        for lx in range(m-1, -1, -1):
+            # print(lx)
+
+            curr[n-1] = 1
+            
+            for ly in range(n-2, -1, -1):
+                # print(lx, ly)
+                # print('neighbors, right, bottom', curr[ly+1], prev[ly])
+                curr[ly] = curr[ly+1] + prev[ly]
+            # print('final curr', curr)
+            prev = curr
+        
+        return curr[0]
