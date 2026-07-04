199af46b2762ad20700b0c665b05ced007be0a0e
Setra Wicana <gw.setra@gmail.com>
2026-07-04 20:29:55 +0100

    Add: longest-common-subsequence - submission-10

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-10.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        cache = {}
+
+        def dfs(text1, text2):
+            lentxt1 = len(text1)
+            lentxt2 = len(text2)
+            # print('txt1 txt2', txt1, txt2)
+            if lentxt2 == 0 :
+                # print('txt2 is None')
+                return 0
+            if lentxt2 and lentxt1 == 0:
+                # print('returning 0')
+                return 0
+            if (text1, text2) in cache:
+                return cache[(text1, text2)]
+            
+            if text1[0] == text2[0]:
+                # print('element find', txt1[0])
+                max_result = 1 + dfs(text1[1:], text2[1:])
+                # print('max_result', max_result)
+                # print('returning from txt1 txt2', txt1, txt2)
+                cache[(text1, text2)] = max_result
+                return max_result
+            else:
+                max_result = max(dfs(text1[1:], text2), dfs(text1, text2[1:]))
+                cache[(text1, text2)] = max_result
+                return max_result
+            
+
+            return 0
+
+        return dfs(text1, text2)
