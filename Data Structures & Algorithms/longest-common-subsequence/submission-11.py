cfa30c17046455331f4d68829f6916656870f54f
Setra Wicana <gw.setra@gmail.com>
2026-07-04 23:37:03 +0100

    Add: longest-common-subsequence - submission-11

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-11.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        p1 = p2 = 0
+        maxrow = len(text1)
+        maxcol = len(text2)
+        prev = [0] * (maxcol+1)
+        # print(text1, text2, prev)
+
+        for lrow in range(maxrow):
+            curr = [0] * (maxcol+1)
+
+            for lcol in range(0, maxcol):
+                # print(lrow, lcol)
+                # print(text1[lrow], text2[lcol])
+                if text1[lrow] == text2[lcol]:
+                    # print('match found')
+                    curr[lcol] = prev[lcol-1] + 1
+                else:
+                    curr[lcol] = max(prev[lcol], curr[lcol-1])
+            # print(prev, curr)
+            prev = curr
+            
+        return curr[maxcol-1]
