3b4838ffa74f06079562e9836a1487d222b5aeb1
Setra Wicana <gw.setra@gmail.com>
2026-07-05 20:53:56 +0100

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.py
@@ -0,0 +1,48 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.arr = [None] * capacity
+        self.arrlen = capacity
+        self.first = 0
+        self.last = 0
+        
+
+    def get(self, i: int) -> int:
+        print('inside get', i)
+        return self.arr[i]
+
+    def set(self, i: int, n: int) -> None:
+        print('self set', i, n)
+        self.arr[i] = n
+
+    def pushback(self, n: int) -> None:
+        print('inside pushback', n)
+        if self.last == 0:
+            self.arr[0] = n
+        else:
+            self.arr.append(n)
+        self.last += 1
+
+        print(self.arr)
+
+    def popback(self) -> int:
+        print('inside popback')
+        tmp = self.arr[self.last-1]
+        self.arr[self.last-1] = None
+        self.last -= 1
+        return tmp
+
+    def resize(self) -> None:
+        pass
+
+    def getSize(self) -> int:
+        print('inside getSize')
+        counter = 0
+        for item in self.arr:
+            counter += 1 if item is not None else 0
+        return counter
+    
+    def getCapacity(self) -> int:
+        print('inside getCapacity ')
+        print(self.arr)
+        return len(self.arr)
