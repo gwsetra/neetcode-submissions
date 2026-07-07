203b27b229c83a212b4e67c37c75818d8b3338fb
Setra Wicana <gw.setra@gmail.com>
2026-07-07 21:31:08 +0100

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = {}
+        res = defaultdict(list)
+
+        for item in strs:
+            keymap = [0] * 26
+            for char in item:
+                # print(ord(char))
+                keymap[ord(char)-ord('a')] += 1
+            if tuple(keymap) not in group:
+                group[tuple(keymap)] = [item]
+            else:
+                group[tuple(keymap)].append(item)
+
+
+
+        return (list(group.values()))
