e6c65cdb09a1dafca9b58821505db2082f3860b6
Setra Wicana <gw.setra@gmail.com>
2026-07-07 20:12:57 +0100

    Add: queue - submission-2

---
+++ Data Structures & Algorithms/queue/submission-2.py
@@ -0,0 +1,21 @@
+class Deque:
+    
+    def __init__(self):
+        self.q = deque() 
+
+    def isEmpty(self) -> bool:
+        return len(self.q) == 0
+
+    def append(self, value: int) -> None:
+        self.q.append(value)
+        # print(self.q)
+
+    def appendleft(self, value: int) -> None:
+        self.q.appendleft(value)
+        # print(self.q)
+
+    def pop(self) -> int:
+        return -1 if len(self.q) <= 0 else self.q.pop()
+
+    def popleft(self) -> int:
+        return -1 if len(self.q) <= 0 else self.q.popleft()
