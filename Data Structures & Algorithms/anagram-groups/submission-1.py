a7a8ff9324299efd0f132928c51944ed99771369
Setra Wicana <gw.setra@gmail.com>
2026-07-07 21:00:44 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sets = {}
+        res = []
+
+        for item in strs:
+            asciirep = 0
+            # print(item)
+            for char in item:
+                # print(char)
+                asciirep += ord(char)
+            if asciirep not in sets:
+                sets[asciirep] = [item]
+            else:
+                sets[asciirep].append(item)
+            
+        print(sets)
+
+        for key, val in sets.items():
+            res.append(val)
+            # print(key, val)
+        
+        return res
