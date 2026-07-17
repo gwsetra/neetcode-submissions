6fc90c6272c7da9ded31776d104843c8008f580c
Setra Wicana <gw.setra@gmail.com>
2026-07-17 12:25:27 +0100

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,50 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs == []:
+            print('a')
+            return str()
+        if strs == ['']:
+            print('b')
+            return '|||'
+        # print(strs)
+        # self.decode()
+        print('c')
+        encode = ''
+        for item in strs:
+            print(item)
+            if item == '':
+                encode += str({0:''}) + '|||'
+            else:
+                # encode += str(len(item)) + item + '|||'
+                encode += str({str(len(item)):item}) + '|||'
+        # return '|||'.join(strs)
+        print(encode)
+        return encode
+
+    def decode(self, s: str) -> List[str]:
+        print(s)
+        if s == '':
+            print('1')
+            return []
+        if s == '|||':
+            print('2')
+            return ['']
+        
+        splitted = s.split('|||')
+        res = []
+        print(splitted)
+        for item in splitted:
+            if item == '':
+                continue
+            if item[0] == 0:
+                res.append('')
+            else:
+                print(item)
+                for counts,word in eval(item).items():
+                    # print(x, y)
+                    res.append(word)
+                # print(next(iter(eval(item
+                
+        print(res)
+        return res
