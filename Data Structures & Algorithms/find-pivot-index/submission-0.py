527dce94d52cf758cbfc27763fbb8a8a676886d6
Setra Wicana <gw.setra@gmail.com>
2026-08-02 19:48:32 +0100

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        totalsum = sum(nums)
+        leftsum = 0
+        rightsum = 0
+        print(totalsum)
+
+        for idx in range(len(nums)):
+            # print('***')
+            # print(idx)
+            if idx == 0:
+                rightsum = totalsum
+            elif idx == len(nums)-1:
+                leftsum = totalsum
+            else:
+                leftsum += nums[idx-1]
+            # print(leftsum)
+            # print(totalsum, nums[idx], leftsum)
+            # print(totalsum-nums[idx]-leftsum)
+            if leftsum == totalsum-nums[idx]-leftsum:
+                return idx
+        return -1
