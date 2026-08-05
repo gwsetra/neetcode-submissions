56dcfde6d4e89dbd35b7c3a9b247a89b29893596
Setra Wicana <gw.setra@gmail.com>
2026-08-05 21:43:38 +0100

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        cursum = 0
+        l = 0
+        r = 0
+        maxlen = 0
+
+        while l != len(nums)-1:
+            if r == len(nums):
+                l += 1
+                r = l
+                cursum = 0
+                # print('l r', l, r)
+                continue
+            cursum += nums[r]
+            # print('cursum', cursum)
+            if cursum >= target:
+                # print('resett')
+                maxlen = (r-l)+1
+                l += 1
+                r = l
+                cursum = 0
+                # print('l r', l, r)
+                continue
+            r += 1
+        return maxlen
