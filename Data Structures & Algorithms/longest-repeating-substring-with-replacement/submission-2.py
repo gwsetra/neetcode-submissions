5a9c3be298b5f1a7f82b932bf9553526b265431f
Setra Wicana <gw.setra@gmail.com>
2026-08-09 20:20:53 +0100

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maps = defaultdict(int)
+        l = 0
+        strlen = len(s)
+        wdwlen = 0
+        currmax = None
+        maxval = 0
+
+        for r in range(strlen):
+            # print(s[r])
+            maps[s[r]] += 1
+            # print(max(maps.values()))
+            # print('currmax char',max(maps, key=maps.get))
+            # print((r-l+1), max(maps.values()), k)
+            if (r-l+1) - max(maps.values()) > k:
+                print('inside IFF')
+                maxval = max(maxval, (r-l))
+                maps[s[l]] -= 1
+                l+=1
+        # print(maps)
+        print(maxval)
+        return maxval if maxval > 0 else strlen
