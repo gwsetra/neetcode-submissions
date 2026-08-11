f5ad6200a45292768b7564107195baecbf145fe3
Setra Wicana <gw.setra@gmail.com>
2026-08-11 22:47:21 +0100

    Add: valid-palindrome-ii - submission-28

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-28.py
@@ -0,0 +1,36 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+
+        if len(s) <= 2:
+            return True
+
+        if len(set(s)) == len(s):
+            return False
+        
+        def checksubpalindrome(x, y):
+            # print('inside palindrome')
+            if y > len(s)-1:
+                y = len(s)-1
+            while y > x:
+                if s[y] != s[x]:
+                    return False
+                y-=1
+                x+=1
+
+            return True
+        
+        while r>l:
+            if s[r] != s[l]:
+                res = checksubpalindrome(l, r-1) or checksubpalindrome(l+1, r)
+                # print(checksubpalindrome(l, r-1+1), checksubpalindrome(l+1, r+1) )
+                # print(res)
+                if res is False:
+                    return False
+                else: 
+                    return True
+
+            r-=1
+            l+=1
+        return True
