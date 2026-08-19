3f9dd5489f18e9be5908717d2b4052387784813e
Setra Wicana <gw.setra@gmail.com>
2026-08-19 22:49:52 +0100

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = 0
+        rp = 0
+
+        while rp < len(t):
+            # print(sp, rp)
+            if t[rp] == s[sp]:
+                sp += 1
+                rp += 1
+                continue
+            rp += 1
+        
+        return sp==len(s)
+            
