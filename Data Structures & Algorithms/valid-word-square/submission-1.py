740a2c8689f105746dac15ad4e0445d22d6fe40c
Setra Wicana <gw.setra@gmail.com>
2026-08-29 19:05:11 +0100

    Add: valid-word-square - submission-1

---
+++ Data Structures & Algorithms/valid-word-square/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        lenelem = len(words[0])
+        for x in range(1, len(words)):
+            for y in range(0, min(x, len(words[x]))):
+                print(x,y)
+                try:
+                    if words[x][y] != words[y][x]:
+                        return False
+                except IndexError:
+                    return False
+        return True
+                
