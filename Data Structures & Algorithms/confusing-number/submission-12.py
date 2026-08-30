6f51237b63bf330b6a9430d83e351f2f56e08369
Setra Wicana <gw.setra@gmail.com>
2026-08-30 15:03:37 +0100

    Add: confusing-number - submission-12

---
+++ Data Structures & Algorithms/confusing-number/submission-12.py
@@ -0,0 +1,25 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        maps = {'0':'0', '1':'1', '6':'9', '8':'8', '9':'6'}
+        char = str(n)
+        flags = False
+        newstr = ''
+
+        if len(char)==1:
+            return True if (char[0] in maps) and char[0] not in ('0', '1', '8') else False
+
+        for i in range(len(char)-1, len(char)//2, -1):
+            print(char[i])
+
+            if char[i] not in maps:
+                return False
+            else:
+                print(maps[char[i]], char[len(char)-1-i], maps[char[len(char)-1-i]], char[i])
+                if len(char)%2 == 1 and len(char) // 2 == i:
+                    print('odd number')
+                    continue
+                if (maps[char[i]] == char[len(char)-1-i]) and (maps[char[len(char)-1-i]] == char[i]):
+                    print('trapped here')
+                    return False
+            
+        return True
