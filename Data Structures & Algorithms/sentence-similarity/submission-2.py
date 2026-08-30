da6618904b06633b70709cb685b9f9fbe7630562
Setra Wicana <gw.setra@gmail.com>
2026-08-30 21:19:22 +0100

    Add: sentence-similarity - submission-2

---
+++ Data Structures & Algorithms/sentence-similarity/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        len1 = sum(len(lens) for lens in sentence1)
+        len2 = sum(len(lens) for lens in sentence2)
+        lenall = sum(len(lens2)   for lens in similarPairs for lens2 in lens)
+        print(len1, len2, lenall)
+
+        if lenall == 0 and sentence1 == sentence2:
+            return True
+        elif lenall > 0 and lenall != len1+len2:
+            return False
+        else:
+            for i in range(len(similarPairs)):
+                # print(similarPairs[i])
+                # print(similarPairs[i][0] != sentence1[i] and similarPairs[i][0] != sentence2[i])
+                # print(similarPairs[i][1] != sentence1[i] and similarPairs[i][1] != sentence2[i])
+                if (similarPairs[i][0] != sentence1[i] and similarPairs[i][0] != sentence2[i]) and (similarPairs[i][1] != sentence1[i] and similarPairs[i][1] != sentence2[i]):
+                    return False
+        return True
