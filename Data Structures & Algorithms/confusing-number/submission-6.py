631197cffd2616eb96bfef104fddfbfc1962a04f
Setra Wicana <gw.setra@gmail.com>
2026-08-30 14:46:07 +0100

    Add: confusing-number - submission-6

---
+++ Data Structures & Algorithms/confusing-number/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        maps = {'0':0, '1':'1', '6':'9', '8':'8', '9':'6'}
+        char = str(n)
+        flags = False
+        newstr = ''
+
+        if len(char)==1:
+            return True if char[0] in maps else False
+
+        for i in range(len(char)-1, -1, -1):
+            # print(char[i])
+
+            if char[i] not in maps:
+                return False
+            else:
+                # print(maps[char[i]], char[len(char)-1-i])
+                if maps[char[i]] == char[len(char)-1-i]:
+                    return False
+            
+        return True
