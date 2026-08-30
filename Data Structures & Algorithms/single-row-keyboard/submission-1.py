40bec3bc6c8d5ae3f18ed0e956491e2924e65f43
Setra Wicana <gw.setra@gmail.com>
2026-08-30 22:41:40 +0100

    Add: single-row-keyboard - submission-1

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        maps = {}
+        rangesum = 0
+        prev = 0
+
+        for i in range(len(keyboard)):
+            maps[keyboard[i]] = i
+        
+        # print(maps)
+        for i in range(len(word)):
+            # print(maps[word[i]])
+            rangesum += abs(maps[word[i]]-prev)
+            prev = maps[word[i]]
+        # print(rangesum)
+        return rangesum
