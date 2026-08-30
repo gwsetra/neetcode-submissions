e8bbd7ed235082a9b0470e1ab4095cb7bbf222f7
Setra Wicana <gw.setra@gmail.com>
2026-08-30 21:47:21 +0100

    Add: sentence-similarity - submission-12

---
+++ Data Structures & Algorithms/sentence-similarity/submission-12.py
@@ -0,0 +1,24 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        cnt = 0
+        combinedset = set(sentence1+sentence2)
+        shortpairs = [pair for pair in similarPairs if set(pair).issubset(combinedset) ]
+        # print(combinedset, shortpairs)
+
+        if (len(similarPairs) == 0 and sentence1 == sentence2) or sentence1 == sentence2:
+            return True
+        if len(sentence1) != len(sentence2):
+            # print('here')
+            return False
+        else:
+            for iters in range(len(sentence1)):
+                for i in range(len(shortpairs)):
+                    # print(similarPairs[i])
+                    if sentence1[iters] == sentence2[iters]:
+                        cnt+=1
+                        # print('found')
+                        break
+                    if (shortpairs[i][0] == sentence1[iters] or shortpairs[i][0] == sentence2[iters]) and (shortpairs[i][1] == sentence1[iters] or shortpairs[i][1] == sentence2[iters]):
+                        cnt+=1
+                        break
+        return cnt == len(sentence1)
