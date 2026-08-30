c16685a217d65c61083f1f92871e844031924bd3
Setra Wicana <gw.setra@gmail.com>
2026-08-30 22:52:21 +0100

    Add: palindrome-permutation - submission-1

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        maps = defaultdict(int)
+        summap = 0
+        lenstr = len(s)
+
+        for i in range(len(s)):
+            maps[s[i]] += 1
+        print(maps)
+        # check odds
+        if lenstr % 2 == 1:
+            print('ODD')
+            # print(len(maps), (lenstr/2)+1)
+            summap += sum(val[1] for val in maps.items())
+            # print(summap)
+            # if len(maps) == (lenstr//2)+1:
+            #     return True
+            if summap == lenstr:
+                return True
+        # check even
+        elif lenstr % 2 == 0:
+            print('EVEN')
+            if len(maps) == lenstr/2:
+                return True
+        return False
