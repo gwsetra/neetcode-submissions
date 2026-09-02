171835b2a4e01783fbc196deacb42c78d4247e9d
Setra Wicana <gw.setra@gmail.com>
2026-09-02 22:00:07 +0100

    Add: perform-string-shifts - submission-5

---
+++ Data Structures & Algorithms/perform-string-shifts/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        net = 0
+        
+        for i in range(len(shift)):
+            if shift[i][0] == 0:
+                net -= shift[i][1]
+            else:
+                net += shift[i][1]
+            # print(net)
+
+        if abs(net) > len(s):
+            net = net % len(s)
+        print(net)
+        if net > 0:
+            return s[-net:]+s[:-net]
+        elif net < 0:
+            print(s[net:], '*', s[:net])
+            return s[net+1:]+s[:net+1]
+        else:
+            return s
+        
