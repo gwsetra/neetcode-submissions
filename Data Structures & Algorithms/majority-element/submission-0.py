8a188ddcce58a0143146fe731b6009380f1d9279
Setra Wicana <gw.setra@gmail.com>
2026-09-04 22:57:59 +0100

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maps = defaultdict(int)
+
+        for i in range(len(nums)):
+            maps[nums[i]] += 1
+        
+        return max(maps)
