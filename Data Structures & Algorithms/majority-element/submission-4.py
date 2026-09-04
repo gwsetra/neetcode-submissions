b62b717babaa7e4f44946a58da1c9185e97c4a2c
Setra Wicana <gw.setra@gmail.com>
2026-09-04 23:07:03 +0100

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maps = defaultdict(int)
+        mostfreq = 0
+
+        for i in range(len(nums)):
+            maps[nums[i]] += 1
+        
+        # print(maps)
+        return max(maps, key=maps.get)
