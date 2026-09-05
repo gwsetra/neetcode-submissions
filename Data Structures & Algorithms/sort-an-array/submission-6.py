5e07e3f0fe05c7c46c3e20d3e193be3061b1ba57
Setra Wicana <gw.setra@gmail.com>
2026-09-05 19:57:45 +0100

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.py
@@ -0,0 +1,47 @@
+class Solution:
+    # 3-way quick sort
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # print('**')
+        # print(nums)
+
+        lennums = len(nums)
+        lp = 0
+        ep = 0
+        gp = lennums-1
+        cur = 0
+
+        if lennums <= 1:
+            return nums
+
+        pivotnum = nums[lennums//2]
+        # print(pivotnum)
+
+        while ep < gp:
+            # print(nums)
+            # print(lp, ep, gp)
+            # smaller case
+            if nums[ep] < pivotnum:
+                # print('smaller case')
+                tmp = nums[lp]
+                nums[lp] = nums[ep]
+                nums[ep] = tmp 
+                lp += 1
+                ep += 1
+
+            # equal case
+            elif nums[ep] == pivotnum:
+                # print('equal case')
+                ep += 1
+            # bigger case
+            elif nums[ep] > pivotnum:
+                # print('bigger case')
+                tmp = nums[gp]
+                nums[gp] = nums[ep]
+                nums[ep] = tmp 
+                gp -= 1
+            # print(lp, ep, gp)
+        
+        # print(nums)
+        # print(nums[:lp], nums[lp:ep+1], nums[ep+1:])
+        return ((self.sortArray(nums[:lp]) or []) + (nums[lp:ep+1] or []) + (self.sortArray(nums[ep+1:]) or []))
+        # print(nums)
