b2627b055956e4d809d8f402d2d3820802ea46ec
Setra Wicana <gw.setra@gmail.com>
2026-09-06 20:50:00 +0100

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        lower = 0
+        upper = x
+        mid = x // 2
+        ans = None
+
+        while lower <= upper:
+            mid = lower + ((upper-lower) // 2)
+            midsqr = mid*mid
+            # print(lower, mid, upper)
+            # print(midsqr, x)
+            if midsqr == x:
+                return mid
+            if midsqr > x:
+                upper = mid - 1
+            if midsqr < x:
+                # print('increase lower')
+                ans = mid
+                lower = mid + 1
+            # print(lower, mid, upper)
+        return ans
