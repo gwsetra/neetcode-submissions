71941a846c5942deeef57cd430a218179b1f26b5
Setra Wicana <gw.setra@gmail.com>
2026-09-06 16:04:11 +0100

    Add: search-insert-position - submission-22

---
+++ Data Structures & Algorithms/search-insert-position/submission-22.py
@@ -0,0 +1,25 @@
+class Solution:
+    # using while
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        si = 0
+        ei = len(nums)-1
+        mid = ((ei-si) // 2) + si
+        # print(mid, ei)
+        
+        while si <= ei:
+            # print('***')
+            # print(si, mid, ei, target, nums[mid-1])
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                # print('target is smaller than mid')
+                ei = mid-1
+            elif target > nums[mid]:
+                # print('target is bigger than mid')
+                si = mid + 1
+            mid = ((ei-si) // 2) + si
+        #     print(si, mid, ei, target, nums[mid-1])
+        # print(si, mid, ei)
+
+
+        return si
