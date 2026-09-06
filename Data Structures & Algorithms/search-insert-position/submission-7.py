4f24d24b2bc8c0b70e3b633c5690f45c9e2538bd
Setra Wicana <gw.setra@gmail.com>
2026-09-06 15:29:44 +0100

    Add: search-insert-position - submission-7

---
+++ Data Structures & Algorithms/search-insert-position/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # print(target)
+
+        def findsub( si, ei):
+            # print('inside findsub', si, ei)
+            mid = ((ei-si) // 2) + si
+            # print(mid)
+
+            if target == nums[mid]:
+                # print('exact element match found')
+                return mid
+            
+            if si == ei:
+                # print('si == ei')
+                # print(nums[ei], target)
+                if target <= nums[ei] :
+                    return ei
+                
+                return ei+1
+
+            elif target < nums[mid]:
+                # print('target < mid', target, nums[mid])
+                return findsub(si, mid-1)
+            
+            elif target > nums[mid]:
+                # print('target > mid', target, nums[mid])
+                return findsub(mid+1, ei)
+        
+        return findsub(0, len(nums)-1)
