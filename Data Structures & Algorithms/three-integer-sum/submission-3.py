ca6c204d1db30bd932ceb74ed7d8ade1da86c014
Setra Wicana <gw.setra@gmail.com>
2026-09-07 22:27:32 +0100

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    # sorting solution
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        numlen = len(nums)-1
+        lp = 1
+        rp = numlen
+        nums.sort()
+        res = []
+        print(nums)
+
+        for i in range(0, numlen-1):
+            if i > 0:
+                if nums[i] == nums[i-1]:
+                    print('same first element found')
+                    continue
+            lp = i + 1
+            rp = numlen
+            loop = 0
+            while lp < rp:
+                tmp = nums[i] + nums[lp] + nums[rp]
+                if tmp == 0:
+                    res.append([nums[i], nums[lp], nums[rp]])
+                    rp -= 1
+                    lp += 1
+                # print(tmp)
+                if tmp > 0:
+                    rp -= 1
+                elif tmp < 0:
+                    lp += 1
+                loop += 1
+        return res
