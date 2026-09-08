b6a3e59a500ba1a7786ffe9d286cac89a5001f54
Setra Wicana <gw.setra@gmail.com>
2026-09-08 21:17:56 +0100

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        maps = defaultdict(int)
+        res = []
+
+        for i in range(len(nums)):
+            maps[nums[i]] +=1
+        print(i, i/3, maps)
+        for item, ocur in maps.items():
+            if ocur > math.ceil(i/3):
+                res.append(item)
+        return res
