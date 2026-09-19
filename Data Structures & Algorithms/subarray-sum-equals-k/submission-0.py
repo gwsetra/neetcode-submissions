30b59a9fba5f413399f1a1f20a8a5b2d99e6ae84
Setra Wicana <gw.setra@gmail.com>
2026-09-19 15:35:31 +0100

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        self.prefixsum = defaultdict(int)
+        print(self.prefixsum)
+        self.cursum = 0
+        count = 0
+        
+        self.prefixsum[0] = 1
+        for i in range(len(nums)):
+            self.cursum += nums[i]
+            prevsum = self.cursum - k
+
+            if prevsum in self.prefixsum:
+                count += self.prefixsum[prevsum]
+
+            self.prefixsum[self.cursum] += 1
+            print(self.cursum, prevsum, self.prefixsum, count)
+        #     print(self.prefixsum)
+        # print(nums)
+        # print(self.prefixsum)
