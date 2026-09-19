9302516f0664e4acdc9153e5c4a82fb06270ec52
Setra Wicana <gw.setra@gmail.com>
2026-09-19 20:52:29 +0100

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        print(people)
+        lp = 0
+        rp = len(people)-1
+        trip = 0
+
+        while lp <= rp:
+            tmplimit = limit
+            if tmplimit - people[rp] >= 0:
+                tmplimit -= people[rp]
+                rp -= 1
+            if tmplimit - people[lp] >= 0:
+                lp += 1
+            trip += 1
+
+        return(trip)
