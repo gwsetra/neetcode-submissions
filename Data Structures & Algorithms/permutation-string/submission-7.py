eea9665b5870992cf7f25a4409316ac533de2257
Setra Wicana <gw.setra@gmail.com>
2026-09-20 16:23:53 +0100

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    # optimised solution
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        counts1 = [0] * 26
+        counts2 = [0] * 26
+        s1counter = 0
+        # print(ord('a'))
+        for i in range(len(s2)):
+            if i >= len(s1):
+                print('start popping up')
+                counts2[ord(s2[i-len(s1)])-ord('a')] -= 1
+            # print(i, ord(s1[min(i, len(s1)-1)]), ord(s2[i]))
+            if s1counter < len(s1):
+                counts1[ord(s1[i])-ord('a')] += 1
+            counts2[ord(s2[i])-ord('a')] += 1
+            s1counter += 1
+            print(counts1)
+            print(counts2)
+            if (counts1 == counts2 and i >= len(s1)):
+                return True
+        if counts1 == counts2:
+            return True
+        return False
