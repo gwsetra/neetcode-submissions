77fa9a433fe9729b00a7db6013dff227758bb8e6
Setra Wicana <gw.setra@gmail.com>
2026-09-20 15:24:17 +0100

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1set = set(s1)
+        # print(len(s2), len(s1), len(s2)-len(s1))
+
+        for i in range(len(s2)-len(s1)+1):
+            # print(s1set, set(s2[i:i+len(s1)]), s1set == set(s2[i:i+len(s1)]))
+            if s1set == set(s2[i:i+len(s1)]):
+                return True
+        return False
